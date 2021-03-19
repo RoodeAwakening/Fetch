12a8f62926e9f2da7bb04b7b5008c1569414bc44
Derek Roode <derek.i.roode@gmail.com>
2021-03-19 18:57:59 -0400

    more db

--- app/models/tag_post.py
+++ app/models/tag_post.py
@@ -1,13 +1,13 @@
 from .db import db
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Text
+
 
 
 class Tag_Post(db.Model):
   __tablename__='tags_posts'
 
   id = db.Column(db.Integer, primary_key = True)
-  tagId = db.Column(db.Integer,ForeignKey("tag.id"), nullable = False )
-  postId = db.Column(db.Integer,ForeignKey("post.id"), nullable = False)
+  tagId = db.Column(db.Integer,db.ForeignKey("tag.id"), nullable = False )
+  postId = db.Column(db.Integer,db.ForeignKey("post.id"), nullable = False)
 # NEED TO ADD UNIQUE
 
   def to_dict(self):
