22c62782950a83cf90037ba5eb8e4782078aacaf
Derek Roode <derek.i.roode@gmail.com>
2021-03-19 18:39:42 -0400

    added imports for db

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,8 @@
 from .db import db
 from .user import User
+from .comment import Comment
+from .follower import follower
+from .post import Post
+from .tag import Tag
+from .tags_post import Tag_Post
+
