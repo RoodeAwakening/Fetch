e04be4dfa6ce3e89d9ba5bd938c134922c3a81ad
Bryan Burns <bryanburns93@gmail.com>
2021-03-22 19:11:13 -0400

    update post route

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.post_routes import post_routes
-from .api.feed_routes import feed_routes
 
 from .seeds import seed_commands
 
@@ -33,8 +32,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(post_routes, url_prefix='/api.post')
-app.register_blueprint(feed_routes, url_prefix='/api/feed')
+app.register_blueprint(post_routes, url_prefix='/api/posts')
+
 db.init_app(app)
 Migrate(app, db)
 
--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -4,19 +4,31 @@ from flask_login import login_required, current_user
 from app.models import Post, User
 from app.forms import UploadForm
 
-post_routes = Blueprint('post', __name__)
+post_routes = Blueprint('posts', __name__)
+
+# Create a new post
 
 
-@post_routes.route('/', methods=['POST'])
+@post_routes.route('/', methods=['GET', 'POST'])
 # @login_required()
 def post():
     form = UploadForm()
+    if form.validate_on_submit():
+        post = Post(
+            userId=1,
+            photo=form.file.data,
+            caption=form.data['caption']
+        )
+        return post.to_dict()
+    posts = []
+    for post in Post.query.all():
+        posts.append(post.to_dict())
+    return jsonify(posts)
+# Get a post by id
 
-    post = Post(
-        userId=1,
-        photo=form.file.data,
-        caption=form.data['caption']
-    )
+
+@post_routes.route('/<int:id>')
+# @login_required
+def getPostById(id):
+    post = Post.query.get(id)
     return post.to_dict()
-
-    return jsonify(current_user.to_dict())
