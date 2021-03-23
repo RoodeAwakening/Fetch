c222d93a57a6cbc84ffdba9860f01d8bb2bf5cca
Bryan Burns <bryanburns93@gmail.com>
2021-03-23 05:22:20 -0400

    updated post routes

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, json, jsonify, request
 from flask_login import login_required, current_user
 
-from app.models import Post, User, Comment, db
+from app.models import db, Post, User, Like, Comment, Tag_Post
 from app.forms import UploadForm
 
 post_routes = Blueprint('posts', __name__)
@@ -12,42 +12,73 @@ post_routes = Blueprint('posts', __name__)
 @post_routes.route('/', methods=['GET', 'POST'])
 # @login_required()
 def post():
-    form = UploadForm()
-    if form.validate_on_submit():
-        post = Post(
-            userId=1,
-            photo=form.file.data,
-            caption=form.data['caption']
-        )
-        return post.to_dict()
-    posts = []
-    for post in Post.query.all():
-        posts.append(post.to_dict())
-    return jsonify(posts)
-# Get a post by id
+    m = request.method
+    if m == 'GET':  # Get a list of posts
+        posts = []
+        for post in Post.query.all():
+            posts.append(post.to_dict())
+        return jsonify(posts)
+    elif m == 'POST':  # Create a new post
+        form = UploadForm()
+        if form.validate_on_submit():
+            post = Post(
+                userId=1,
+                photo=form.file.data,
+                caption=form.data['caption']
+            )
+            db.session.add(post)
+            db.session.commit()
+        return post.to_dict() if post else 'Invalid operation'
 
 
 @post_routes.route('/<int:id>', methods=['GET', 'DELETE'])
 # @login_required
 def postById(id):
-    if request.method == 'GET':  # Get a data for a given post
+    m = request.method
+    if m == 'GET':  # Get a data for a given post
         post = Post.query.get(id)
         return post.to_dict()
-    elif request.method == 'DELETE':  # Delete a given post
-        Post.query.get(id).delete()
+    elif m == 'DELETE':  # Delete a given post
+        success = Post.query.filter(Post.id == id).delete()
         db.session.commit()
-        return 'Comment deleted.'
+        return jsonify('Successfully deleted.' if success else 'Invalid operation.')
+
+
+@post_routes.route('/<int:id>/likes', methods=['GET', 'POST', 'DELETE'])
+# @login_required
+def likesByPostId(id):
+    m = request.method
+    if m == 'GET':  # Get number of likes for a given post
+        likes = []
+        for like in Like.query.filter(Like.postId == id).all():
+            likes.append(like.to_dict())
+        res = {
+            'count': len(likes),
+            'likes': likes
+        }
+        return jsonify(res)
+    elif m == 'POST':  # Create a new like for the given post
+        # userId should come from currentUser
+        # userId = current_user.id???
+        like = Like(postId=id, userId=2)
+        db.session.add(like)
+        db.session.commit()
+        return jsonify(like.to_dict())
+    elif m == 'DELETE':  # Delete a like for the given post
+        db.session.commit()
+        return jsonify()
 
 
 @post_routes.route('/<int:id>/comments', methods=['GET', 'POST'])
 # @login_required
 def commentsByPostId(id):
-    if request.method == 'GET':  # Get comments for given post
+    m = request.method
+    if m == 'GET':  # Get comments for given post
         comments = []
         for comment in Comment.query.filter(Comment.postId == id).all():
             comments.append(comment.to_dict())
         return jsonify(comments)
-    elif request.method == 'POST':  # Create new comment for given post
+    elif m == 'POST':  # Create new comment for given post
         content = request.json['content']
         # userId should come from currentUser
         # userId = current_user.id???
@@ -57,4 +88,16 @@ def commentsByPostId(id):
         return jsonify(comment.to_dict())
 
 
-@post_routes.route()
+@post_routes.route('/<int:id>/tags', methods=['GET', 'POST', 'DELETE'])
+# @login_required
+def tagsByPostId(id):
+    m = request.method
+    if m == 'GET':  # Get the tags for a given post
+        tags = []
+        for tag in Tag_Post.query.filter(Tag_Post.postId == id).all():
+            tags.append(tag.to_dict())
+        return jsonify(tags)
+    elif m == 'POST':  # Create a new tag for the given post
+        return 'POST TAG'
+    elif m == 'DELETE':  # Delete a tag from the given post
+        return 'DELETE TAG'
