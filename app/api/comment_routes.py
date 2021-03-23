c5dc48301efc9107b19bff6ba9e7ebf15c862392
Bryan Burns <bryanburns93@gmail.com>
2021-03-23 03:26:54 -0400

    added comment and post backend routes

---
+++ app/api/comment_routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+
+from app.models import Comment
+
+comment_routes = Blueprint('comments',  __name__)
+
+
+@comment_routes.route('/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def comment():
--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,14 +1,14 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 
-from app.models import Post, User
+from app.models import Post, User, Comment, db
 from app.forms import UploadForm
 
 post_routes = Blueprint('posts', __name__)
 
+
+# Get posts
 # Create a new post
-
-
 @post_routes.route('/', methods=['GET', 'POST'])
 # @login_required()
 def post():
@@ -27,8 +27,34 @@ def post():
 # Get a post by id
 
 
-@post_routes.route('/<int:id>')
+@post_routes.route('/<int:id>', methods=['GET', 'DELETE'])
 # @login_required
-def getPostById(id):
-    post = Post.query.get(id)
-    return post.to_dict()
+def postById(id):
+    if request.method == 'GET':  # Get a data for a given post
+        post = Post.query.get(id)
+        return post.to_dict()
+    elif request.method == 'DELETE':  # Delete a given post
+        Post.query.get(id).delete()
+        db.session.commit()
+        return 'Comment deleted.'
+
+
+@post_routes.route('/<int:id>/comments', methods=['GET', 'POST'])
+# @login_required
+def commentsByPostId(id):
+    if request.method == 'GET':  # Get comments for given post
+        comments = []
+        for comment in Comment.query.filter(Comment.postId == id).all():
+            comments.append(comment.to_dict())
+        return jsonify(comments)
+    elif request.method == 'POST':  # Create new comment for given post
+        content = request.json['content']
+        # userId should come from currentUser
+        # userId = current_user.id???
+        comment = Comment(postId=id, userId=2, content=content)
+        db.session.add(comment)
+        db.session.commit()
+        return jsonify(comment.to_dict())
+
+
+@post_routes.route()
