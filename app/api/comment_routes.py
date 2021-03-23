493e05fd89a7e781a0620edc0ef246506fbc5530
Bryan Burns <bryanburns93@gmail.com>
2021-03-23 04:00:20 -0400

    add/finish comment routes

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,11 +1,27 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
 
-from app.models import Comment
+from app.models import db, Comment
 
 comment_routes = Blueprint('comments',  __name__)
 
 
 @comment_routes.route('/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 # @login_required
-def comment():
+def comment(id):
+    m = request.method
+    if m == 'GET':  # Get data for a given comment
+        comment = Comment.query.get(id)
+        return jsonify(comment.to_dict() if comment else 'Invalid operation.')
+    elif m == 'PATCH':  # Update the content of a given comment
+        comment = Comment.query.get(id)
+        if comment:
+            content = request.json['content']
+            comment.content = content
+            db.session.commit()
+        return jsonify(comment.to_dict() if comment else 'Invalid operation.')
+
+    elif m == 'DELETE':  # Delete a given comment
+        success = Comment.query.filter(Comment.id == id).delete()
+        db.session.commit()
+        return jsonify('Successfully deleted.' if success else 'Invalid operation.')
