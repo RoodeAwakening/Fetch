9cfd16c07824332b2f269333099f7656512185cf
Justin Payne <justinppayne@gmail.com>
2021-03-24 18:59:47 -0500

    store data selected for use in post component

--- react-app/src/components/FeedPage/FeedPage.js
+++ react-app/src/components/FeedPage/FeedPage.js
@@ -5,17 +5,24 @@ import "./FeedPage.css";
 import Post from "../Post/Post";
 
 export default function FeedPage() {
+  const postData = useSelector((state) => state.posts);
+  console.log("POST_DATA:", postData);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(posts())
+    dispatch(posts());
   }, [dispatch]);
+
+  const postInfo = [];
+  for (const post in postData) {
+    postInfo.push(postData[post]);
+  }
 
   return (
     <div className="FeedPage_posts">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {/* {postData.map((post) => console.log(post))} */}
+      {postInfo.map((post) => {
+        return <Post postInfo={post} />;
+      })}
     </div>
   );
 }
--- react-app/src/components/Post/Post.js
+++ react-app/src/components/Post/Post.js
@@ -1,55 +1,59 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Link, useHistory, useParams } from 'react-router-dom'
-import './Post.css'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useHistory, useParams } from "react-router-dom";
+import "./Post.css";
 
-import { post } from '../../store/posts'
+import { post } from "../../store/posts";
 
-export default function Post() {
-	const dispatch = useDispatch()
-	const history = useHistory()
-	const { postId } = useParams()
+export default function Post({ postInfo }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { postId } = useParams();
+  console.log("POSTINFO", postInfo);
+  useEffect(async () => {
+    dispatch(post(postId));
+  });
 
-	useEffect(async () => {
-		dispatch(post(postId))
-	})
-
-	return (
-		<div className="Post">
-			<div className="Post_header">
-				<img
-					className="Post_avatar"
-					alt="post-photo"
-					// src={user.profilePhoto}
-					src="https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg"
-				></img>
-				<h4>Tom</h4>
-				{/* <h4>{userName}</h4> */}
-			</div>
-			{/* <img className="Post_photo" alt="post-photo" src={post.photo}></img> */}
-			<img className="Post_photo" alt="post-photo" src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
-			<div className="Post_photo-footer">
-				<span className="Post_likes">
-					<i className="far fa-heart" id="Post_heart"></i>
-				</span>
-				<span className="Post_comment-icon">
-					<Link className="Post_comment-icon" to="/post">
-						<i className="far fa-comment" id="Post_comment-bubble"></i>
-					</Link>
-				</span>
-				<p className="Post_caption">
-					{/* <strong>{userName}</strong> {caption} */}
-					<strong>Tom</strong> My furrevver fren
-				</p>
-			</div>
-			<div className="Post_comment-container">
-				<ul className="Post_comments-list">
-					<li className="Post_comment">Comment list...</li>
-					<li className="Post_comment">Comment list...</li>
-					<li className="Post_comment">Comment list...</li>
-					<li className="Post_comment">Comment list...</li>
-				</ul>
-			</div>
-		</div>
-	)
+  return (
+    <div className="Post">
+      <div className="Post_header">
+        <img
+          className="Post_avatar"
+          alt="post-photo"
+          // src={user.profilePhoto}
+          src="https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg"
+        ></img>
+        <h4>Tom</h4>
+        {/* <h4>{userName}</h4> */}
+      </div>
+      {/* <img className="Post_photo" alt="post-photo" src={post.photo}></img> */}
+      <img
+        className="Post_photo"
+        alt="post-photo"
+        src={postInfo.post.photo}
+      ></img>
+      <div className="Post_photo-footer">
+        <span className="Post_likes">
+          <i className="far fa-heart" id="Post_heart"></i>
+        </span>
+        <span className="Post_comment-icon">
+          <Link className="Post_comment-icon" to="/post">
+            <i className="far fa-comment" id="Post_comment-bubble"></i>
+          </Link>
+        </span>
+        <p className="Post_caption">
+          {/* <strong>{userName}</strong> {caption} */}
+          <strong>Tom</strong> {postInfo.post.caption}
+        </p>
+      </div>
+      <div className="Post_comment-container">
+        <ul className="Post_comments-list">
+          <li className="Post_comment">Comment list...</li>
+          <li className="Post_comment">Comment list...</li>
+          <li className="Post_comment">Comment list...</li>
+          <li className="Post_comment">Comment list...</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
