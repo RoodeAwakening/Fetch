c5f00dc8ec59141d49c181665fcade76531c31c3
Bryan Burns <bryanburns93@gmail.com>
2021-03-27 18:05:23 -0400

    add store to post component

--- react-app/src/components/Post/Post.js
+++ react-app/src/components/Post/Post.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useSelector } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { createLike, removeLike } from '../../store/posts'
