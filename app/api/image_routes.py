0dd7ad8886c969a59e12f44e1a6478a901600e1f
Bryan Burns <bryanburns93@gmail.com>
2021-03-27 04:56:42 -0400

    start dog detection route

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
 from .s3_helpers import (
     upload_file_to_s3, allowed_file, get_unique_filename)
+from google.cloud import vision
+from google.cloud.vision_v1 import types
+
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'dog-instagram-aa-project-a065f808e7b8.json'
 
 image_routes = Blueprint("images", __name__)
 
 
 @image_routes.route("", methods=["POST"])
-# @login_required
 def upload_image():
 
     if "image" not in request.files:
@@ -17,7 +20,7 @@ def upload_image():
 
     if not allowed_file(image.filename):
         return {"errors": "file type not permitted"}, 400
-    
+
     image.filename = get_unique_filename(image.filename)
 
     upload = upload_file_to_s3(image)
@@ -30,5 +33,24 @@ def upload_image():
 
     url = upload["url"]
 
-  
+
     return {"url": url}
+
+@image_routes.route("/dog-detect")
+def isValid():
+    image = types.Image()
+    image.source.image_uri = request.args.get('url')
+
+    client = vision.ImageAnnotatorClient()
+    response_label = client.label_detection(image=image)
+
+    #Testing
+    # for label in response_label.label_annotations:
+    #     print({'label': label.description, 'score': label.score})
+
+    for label in response_label.label_annotations:
+       if 'Dog' in label.description and label.score > 0.90:
+            return jsonify({'dogFound': True})
+
+    return jsonify({'dogFound': False})
+
