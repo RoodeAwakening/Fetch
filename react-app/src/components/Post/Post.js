ba77f72b62be2b741c216ecfeab7d56c6168c389
Bryan Burns <bryanburns93@gmail.com>
2021-03-27 18:49:29 -0400

    merge with main

--- react-app/src/components/Post/Post.js
+++ react-app/src/components/Post/Post.js
@@ -1,5 +1,5 @@
-import React, { useState, useSelector } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { createLike, removeLike } from '../../store/posts'
 import { createComment } from '../../store/posts'
